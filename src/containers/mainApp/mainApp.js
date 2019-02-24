cece70e7f0443c57b7e9f17e015ba2f94ab62948
Piepieonline <piepieonline@gmail.com>
2019-02-24 21:13:31 +1100

    Moved the content object to a higher level, outside of the main component

--- src/containers/mainApp/mainApp.js
+++ src/containers/mainApp/mainApp.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 
-import ProductImageBlack from '../../assets/ath-msr7-black.jpg';
-import ProductImageBrown from '../../assets/ath-msr7-brown.jpg';
-
 import TabController from '../../components/tabController/tabController';
 import ItemPrice from '../../components/itemPrice/itemPrice';
 import OptionSet from '../../components/optionSet/optionSet';
@@ -15,52 +12,14 @@ class MainApp extends Component {
     constructor(props) {
         super(props);
 
-        this.content = {
-            title: 'Audio-Technica ATH-MSR7',
-            subtitle: '2017 Best Headphones of the Year Award Winner',
-            prices: {
-                standard: '89.99',
-                sale: '59.99'
-            },
-            tabs: [
-                {
-                    title: 'Description',
-                    content: (<p>
-                        Description Lorem ipsum. Sed ultrices ornare nisl id fermentum. Morbi auctor nisl ac aliquam tempus. Nam mollis odio vel finibus placerat. Morbi sit amet leo eget odio venenatis sollicitudin. Sed sed congue magna, eget volutpat dolor. Donec facilisis mollis ante nec lacinia. Sed eleifend dictum est, ut fringilla leo gravida quis. Donec efficitur bibendum eros, eu lobortis nunc maximus ut. Maecenas suscipit eleifend bibendum. Mauris et pellentesque purus. Curabitur in nisl eu diam euismod mollis. Donec tincidunt dapibus sapien, sed commodo metus semper eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet augue eget eleifend ultrices. Etiam faucibus eu felis sit amet tincidunt.
-                </p>)
-                },
-                {
-                    title: 'Details',
-                    content: (<p>
-                        Details Lorem ipsum. Morbi nec velit non ligula tristique vulputate et eget neque. Vivamus tincidunt turpis a pretium accumsan. Donec consectetur, felis eu pretium maximus, libero nulla eleifend leo, sit amet aliquet lacus eros vel nisi. Donec nisl mauris, scelerisque in nunc et, fermentum fringilla odio. Curabitur arcu enim, molestie non bibendum suscipit, iaculis in urna. Vestibulum mollis odio sit amet nunc tincidunt, nec porta est volutpat. Donec id sem a massa eleifend iaculis. Duis a velit efficitur, rhoncus quam sit amet, efficitur risus. Cras nec elit non ipsum sollicitudin pretium. In egestas suscipit dui nec maximus. Donec et ipsum ornare, rhoncus risus at, ultrices ligula. Nulla cursus leo nec condimentum convallis.
-                </p>)
-                }
-            ],
-            options: [
-                {
-                    type: 'Colours',
-                    values: [
-                        {
-                            value: 'Black',
-                            productImage: ProductImageBlack
-                        },
-                        {
-                            value: 'Brown',
-                            productImage: ProductImageBrown
-                        }
-                    ]
-                }
-            ]
-        };
-
         this.state = {
-            selectedImage: ProductImageBlack
+            selectedImage: this.props.content.options.filter((option) => option.type === 'Colours')[0].values[0].productImage
         };
     }
 
-    selectImage = (colourVariantIndex) => {
+    selectImage = (type, colourVariantIndex) => {
         this.setState({
-            selectedImage: this.content.options[0].values[colourVariantIndex].productImage
+            selectedImage: this.props.content.options.filter((option) => option.type === type)[0].values[colourVariantIndex].productImage
         });
     }
 
@@ -73,13 +32,13 @@ class MainApp extends Component {
                         <a href="#" className="back-link">All products</a> { /* eslint-disable-line jsx-a11y/anchor-is-valid */}
                     </div>
                     <div className="item-header">
-                        <ItemTitle title={this.content.title} subtitle={this.content.subtitle} />
+                        <ItemTitle title={this.props.content.title} subtitle={this.props.content.subtitle} />
                     </div>
                     <div className="item-details">
-                        <TabController tabs={this.content.tabs} />
-                        <ItemPrice price={this.content.prices.standard} salePrice={this.content.prices.sale} />
-                        <OptionSet options={this.content.options} changeOption={(type, value) => {
-                            this.selectImage(value);
+                        <TabController tabs={this.props.content.tabs} />
+                        <ItemPrice price={this.props.content.prices.standard} salePrice={this.props.content.prices.sale} />
+                        <OptionSet options={this.props.content.options} changeOption={(type, value) => {
+                            this.selectImage(type, value);
                         }} />
                     </div>
                     <div className="cart-controls">
--- src/index.js
+++ src/index.js
@@ -3,5 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MainApp from './containers/mainApp/mainApp';
 
-ReactDOM.render(<MainApp />, document.getElementById('root'));
+import ProductImageBlack from './assets/ath-msr7-black.jpg';
+import ProductImageBrown from './assets/ath-msr7-brown.jpg';
+
+ReactDOM.render(<MainApp content={
+    {
+        title: 'Audio-Technica ATH-MSR7',
+        subtitle: '2017 Best Headphones of the Year Award Winner',
+        prices: {
+            standard: '89.99',
+            sale: '59.99'
+        },
+        tabs: [
+            {
+                title: 'Description',
+                content: (<p>
+                    Description Lorem ipsum. Sed ultrices ornare nisl id fermentum. Morbi auctor nisl ac aliquam tempus. Nam mollis odio vel finibus placerat. Morbi sit amet leo eget odio venenatis sollicitudin. Sed sed congue magna, eget volutpat dolor. Donec facilisis mollis ante nec lacinia. Sed eleifend dictum est, ut fringilla leo gravida quis. Donec efficitur bibendum eros, eu lobortis nunc maximus ut. Maecenas suscipit eleifend bibendum. Mauris et pellentesque purus. Curabitur in nisl eu diam euismod mollis. Donec tincidunt dapibus sapien, sed commodo metus semper eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet augue eget eleifend ultrices. Etiam faucibus eu felis sit amet tincidunt.
+            </p>)
+            },
+            {
+                title: 'Details',
+                content: (<p>
+                    Details Lorem ipsum. Morbi nec velit non ligula tristique vulputate et eget neque. Vivamus tincidunt turpis a pretium accumsan. Donec consectetur, felis eu pretium maximus, libero nulla eleifend leo, sit amet aliquet lacus eros vel nisi. Donec nisl mauris, scelerisque in nunc et, fermentum fringilla odio. Curabitur arcu enim, molestie non bibendum suscipit, iaculis in urna. Vestibulum mollis odio sit amet nunc tincidunt, nec porta est volutpat. Donec id sem a massa eleifend iaculis. Duis a velit efficitur, rhoncus quam sit amet, efficitur risus. Cras nec elit non ipsum sollicitudin pretium. In egestas suscipit dui nec maximus. Donec et ipsum ornare, rhoncus risus at, ultrices ligula. Nulla cursus leo nec condimentum convallis.
+            </p>)
+            }
+        ],
+        options: [
+            {
+                type: 'Colours',
+                values: [
+                    {
+                        value: 'Black',
+                        productImage: ProductImageBlack
+                    },
+                    {
+                        value: 'Brown',
+                        productImage: ProductImageBrown
+                    }
+                ]
+            }
+        ]
+    }
+} />, document.getElementById('root'));
 
