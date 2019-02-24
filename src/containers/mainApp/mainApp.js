ca10c877c59f16bc99ad35c12018991f34e951fd
Piepieonline <piepieonline@gmail.com>
2019-02-24 16:11:25 +1100

    Squashed commit of the following:
    commit 4f65eb7428a522c74de16be423d8cd5e872c457a
    Author: Piepieonline <piepieonline@gmail.com>
    Date:   Sun Feb 24 16:09:11 2019 +1100
    
        Initial commit

---
+++ src/containers/mainApp/mainApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './mainApp.css';
+
+class MainApp extends Component {
+    render() {
+        return (
+            <div>
+                Blank app
+            </div>
+        )
+    }
+}
+
+export default MainApp;
