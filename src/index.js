29cd5728b447bd3eed658d4b6ec42dba1207c891
Piepieonline <piepieonline@gmail.com>
2019-02-24 21:36:04 +1100

    Modifed the provided assets, further fine tuning of the styling. Added a README

--- src/index.js
+++ src/index.js
@@ -18,13 +18,13 @@ ReactDOM.render(<MainApp content={
             {
                 title: 'Description',
                 content: (<p>
-                    Description Lorem ipsum. Sed ultrices ornare nisl id fermentum. Morbi auctor nisl ac aliquam tempus. Nam mollis odio vel finibus placerat. Morbi sit amet leo eget odio venenatis sollicitudin. Sed sed congue magna, eget volutpat dolor. Donec facilisis mollis ante nec lacinia. Sed eleifend dictum est, ut fringilla leo gravida quis. Donec efficitur bibendum eros, eu lobortis nunc maximus ut. Maecenas suscipit eleifend bibendum. Mauris et pellentesque purus. Curabitur in nisl eu diam euismod mollis. Donec tincidunt dapibus sapien, sed commodo metus semper eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam aliquet augue eget eleifend ultrices. Etiam faucibus eu felis sit amet tincidunt.
+                    Description Lorem ipsum. Sed ultrices ornare nisl id fermentum. Morbi auctor nisl ac aliquam tempus. Nam mollis odio vel finibus placerat. Morbi sit amet leo eget odio venenatis sollicitudin. Sed sed congue magna, eget volutpat dolor. Donec facilisis mollis ante nec lacinia. Sed eleifend dictum est, ut fringilla leo gravida quis. Donec efficitur bibendum eros, eu lobortis nunc maximus ut. Maecenas suscipit eleifend bibendum. Mauris et pellentesque purus. Curabitur in nisl eu diam euismod mollis.
             </p>)
             },
             {
                 title: 'Details',
                 content: (<p>
-                    Details Lorem ipsum. Morbi nec velit non ligula tristique vulputate et eget neque. Vivamus tincidunt turpis a pretium accumsan. Donec consectetur, felis eu pretium maximus, libero nulla eleifend leo, sit amet aliquet lacus eros vel nisi. Donec nisl mauris, scelerisque in nunc et, fermentum fringilla odio. Curabitur arcu enim, molestie non bibendum suscipit, iaculis in urna. Vestibulum mollis odio sit amet nunc tincidunt, nec porta est volutpat. Donec id sem a massa eleifend iaculis. Duis a velit efficitur, rhoncus quam sit amet, efficitur risus. Cras nec elit non ipsum sollicitudin pretium. In egestas suscipit dui nec maximus. Donec et ipsum ornare, rhoncus risus at, ultrices ligula. Nulla cursus leo nec condimentum convallis.
+                    Details Lorem ipsum. Morbi nec velit non ligula tristique vulputate et eget neque. Vivamus tincidunt turpis a pretium accumsan. Donec consectetur, felis eu pretium maximus, libero nulla eleifend leo, sit amet aliquet lacus eros vel nisi. Donec nisl mauris, scelerisque in nunc et, fermentum fringilla odio. Curabitur arcu enim, molestie non bibendum suscipit, iaculis in urna. Vestibulum mollis odio sit amet nunc tincidunt, nec porta est volutpat. Donec id sem a massa eleifend iaculis.
             </p>)
             }
         ],
